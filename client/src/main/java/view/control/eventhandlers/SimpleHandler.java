12992a93f8704e80b3aa7ae976e643965cfd9150
FARES-LAP <FARES-LAP@Fares>
2019-02-07 21:36:50 +0200

    Created package structure

---
+++ client/src/main/java/view/control/eventhandlers/SimpleHandler.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.control.eventhandlers;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+
+/**
+ *
+ * @author FARES-LAP
+ */
+public class SimpleHandler implements EventHandler<Event>{
+
+    @Override
+    public void handle(Event event) {
+        
+    }
+    
+}
